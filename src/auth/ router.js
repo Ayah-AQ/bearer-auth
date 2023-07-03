f02bcccf4cfcc714324b66fb9bdd5a8bab3f3fd7
Ayah-AQ <ayoya_1998@hotmail.com>
2023-07-03 01:23:24 +0000

    edit

--- src/auth/ router.js
+++ src/auth/ router.js
@@ -1,37 +1,35 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const { users } = require('./models')
-const router = express.Router()
+const express = require('express');
+const { users } = require('./models');
+const router = express.Router();
 
-const bcrypt = require('bcrypt')
-const basicAuth = require('./middleware/basicAuth')
-const bearerAuth = require('./middleware/bearerAuth')
+const bcrypt = require('bcrypt');
+const basicAuth = require('./middleware/basicAuth');
+const bearerAuth = require('./middleware/bearerAuth');
 
-router.post('/signup', userSignUp)
-router.post('/signin', basicAuth, userSignIn)
-router.get('/secretstuff', bearerAuth, newPage)
+router.post('/signup', signUp);
+router.post('/signin', basicAuth, signIn);
+router.get('/secret', bearerAuth, secret);
 
-
-async function userSignUp(req, res) {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 5)
-    const newUser = await users.create({ 
-        username: username.toLowerCase(),
-        password: hashedPassword
-    })
-    res.status(201).json(newUser)
+async function signUp(req, res) {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 5);
+  const newUser = await users.create({
+    username: username.toLowerCase(),
+    password: hashedPassword,
+  });
+  res.status(201).json(newUser);
 }
 
-function userSignIn(req, res) {
-    res.status(200).json(req.user)
+function signIn(req, res) {
+  res.status(200).json(req.user);
 }
 
-function newPage(req, res) {
-    res.status(200).json({
-        massege: "Welcom to secretstuff page"
-    })
+function secret(req, res) {
+  res.status(200).json({
+    message: "Welcome to secret",
+  });
 }
-
 
 module.exports = router;
--- src/auth/middleware/bearerAuth.js
+++ src/auth/middleware/bearerAuth.js
@@ -1,17 +1,20 @@
-'use strict'
+'use strict';
 
-const { users } = require('../models')
+const { users } = require('../models');
 
-module.exports =async (req, res, next) => {
-
-    if (req.headers.authorization) {
-        const token = req.headers.authorization.split(' ').pop();
-       await users.bearerAuthChecker(token).then(data => {
-            req.user = data
-            next()
-        }).catch(err => {
-            next(err)
-        })
-
-    }
-}
+module.exports = async (req, res, next) => {
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split(' ').pop();
+    await users
+      .bearerAuthChecker(token)
+      .then((data) => {
+        req.user = data;
+        next();
+      })
+      .catch((err) => {
+        next(err);
+      });
+  } else {
+    next({ message: 'Missing token' });
+  }
+};
